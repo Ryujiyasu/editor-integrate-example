341e45ac4d2d5f32daba60ec621c76fdc4ffacbc
RyujiYasukochi <ryuji.yasu@gmail.com>
2025-02-16 05:16:04 +0900

    ua機能追加

--- index.ts
+++ index.ts
@@ -74,7 +74,12 @@ async function getUsername(
 
     
 
-
+    console.log(`site_type: ${site_type}`);
+    console.log(`location_word: ${location_word}`);
+    console.log(`search_word: ${search_word}`);
+    console.log(`search_type: ${search_type}`);
+    console.log(`site_id: ${site_id}`);
+    console.log(`location: ${location}`);
     return await run((
         site_type: number,
         location_word: string,
@@ -84,6 +89,7 @@ async function getUsername(
         location: string
 
     ) => {
+
         const clickAllowButton = (safariProc) => {
             let frontWin = safariProc.windows[0];
 
@@ -115,38 +121,56 @@ async function getUsername(
         }
 
         const searchUrlClick = (safari) => {
-            safari.doJavaScript(`
-                let elements = document.querySelectorAll(".MjjYud");
-                console.log("elements: " + elements.length);
-                let finish = false;
-                for (let i = 0; i < elements.length; i++) {
-                    let a = elements[i].querySelectorAll("a");
-                    console.log("a: " + a);
-                    for (let j = 0; j < a.length; j++) {
-                        if (a[j].href == null) {
-                            continue;
-                        }
-                        console.log("a[j].href: " + a[j].href);
-                        let url = a[j].href.toString();
+            console.log("search_type: " + search_type);
 
-                        console.log("url: " + url);
-
-                        if(url.includes("youtube") || url.includes("twitter") || url.includes("instagram") || url.includes("facebook")) {
-                            continue;
+            // random の場合
+            if (search_type.startsWith("random")) {
+               search_type = search_type.replace("random", "");
+               console.log("search_type: " + search_type);
+               let search_type_num = Number(search_type);
+               if (search_type_num == 0) {
+                   // 1 ~ 10
+                     search_type_num = Math.floor(Math.random() * 10) + 1;
+               }
+                console.log("search_type_num: " + search_type_num);
+                safari.doJavaScript(`
+                    let elements = document.querySelectorAll(".MjjYud");
+                    console.log("elements: " + elements.length);
+                    let finish = false;
+                    let counter = 0;
+                    for (let i = 0; i < elements.length; i++) {
+                        let a = elements[i].querySelectorAll("a");
+                        console.log("a: " + a);
+                        for (let j = 0; j < a.length; j++) {
+                            if (a[j].href == null) {
+                                continue;
+                            }
+                            console.log("a[j].href: " + a[j].href);
+                            let url = a[j].href.toString();
+    
+                            console.log("url: " + url);
+    
+                            if(url.includes("youtube") || url.includes("twitter") || url.includes("instagram") || url.includes("facebook")) {
+                                continue;
+                            }
+                            if(url.includes("google") || url.includes("apple")) {
+                                continue;
+                            }
+                            counter++;
+                            if(counter == ${search_type_num}) {
+                                a[j].click();
+                                finish = true;
+                                break;
+                            }
+  
                         }
-                        if(url.includes("google") || url.includes("apple")) {
-                            continue;
+                        if (finish) {
+                            break;
                         }
-                            
-                        a[j].click();
-                        finish = true;
-                        break;  
                     }
-                    if (finish) {
-                        break;
-                    }
-                }
-            `, {in: safari.windows[0].currentTab()});
+                `, {in: safari.windows[0].currentTab()});
+            }
+            
             globalThis.delay(2);
             safari.doJavaScript(`
                 window.scrollBy({
@@ -209,11 +233,40 @@ async function getUsername(
 
 
 
+            safariProc.menuBars[0].menuBarItems.byName("開発").click();
+            globalThis.delay(0.5); // 少し待たないとメニューが開ききる前に要素取得が走ってしまう
+
+            // 4. 「ファイル」メニューのサブメニュー項目を全て取得
+            let kaihatsuMenu = safariProc.menuBars[0].menuBarItems.byName("開発").menus[0];
+            let kaihatsuMenuItems = kaihatsuMenu.menuItems();
+
+            // 5. コンソールログに各メニュー項目の名前を出力
+            console.log("=== 'ユーザーエージェント ===");
+
+            kaihatsuMenuItems.at(1).click();
+            const kaihatsuSubMenu = kaihatsuMenuItems.at(1).menus[0].menuItems();
+     
+            kaihatsuSubMenu.at(17).click();
+
+            globalThis.delay(2);
+            
+            // keyboard 全選択
+            systemEvents.keystroke("a", { using: "command down" });
+            // delete
+            systemEvents.keystroke("\u0008");
+            // input
+            systemEvents.keystroke("Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 YJApp-IOS jp.co.yahoo.ipn.appli/4.49.0");
+            // enter
+            systemEvents.keyCode(76);
+            globalThis.delay(2);
+
             console.log("=== 完了 ===");
             let tab = safari.windows[0].currentTab();
             globalThis.delay(3);
 
             if (location_word != "") {
+                console.log("site_type: " + site_type);
+
                 if(site_type == 1) {
                     safari.documents[0].url = search_sites[0];
                 }else {
@@ -221,6 +274,8 @@ async function getUsername(
                 }
 
                 console.log("天気を検索");
+                console.log("location_word: " + location_word);
+                globalThis.delay(2);
                 safari.doJavaScript(`
                 document.querySelector("textarea.gLFyf").value = "${location_word}";
                 document.querySelector("textarea.gLFyf").form.submit();
@@ -250,18 +305,32 @@ async function getUsername(
 
                 globalThis.delay(2);
 
-                if(site_type == 1) {
-                    safari.documents[0].url = search_sites[0];
-                }else{
-                    safari.documents[0].url = search_sites[1];
-                }
+                //許可が出ればおす
+                clickAllowButton(safariProc);
 
                 globalThis.delay(2);
+
+                if(site_id == "J1"){
+                    if(site_type == 1) {
+                        safari.documents[0].url = search_sites[0];
+                    }else{
+                        safari.documents[0].url = search_sites[1];
+                    }
+    
+                    globalThis.delay(2);
+
+                }
+
             }else{
-                if(site_type == 1) {
-                    safari.documents[0].url = search_sites[0];
-                }else{
-                    safari.documents[0].url = search_sites[1];
+                if(site_id == "J1"){
+                    if(site_type == 1) {
+                        safari.documents[0].url = search_sites[0];
+                    }else{
+                        safari.documents[0].url = search_sites[1];
+                    }
+    
+                    globalThis.delay(2);
+
                 }
                 globalThis.delay(2);
             }
@@ -277,6 +346,8 @@ async function getUsername(
 
             globalThis.delay(2);
 
+
+            
             safari.doJavaScript(`
             document.querySelector("a[class='zReHs']")[0].click();
             `, {in: tab});
@@ -334,7 +405,7 @@ async function getUsername(
 
         return "Done!";
 
-    });
+    }, site_type, location_word, search_word, search_type, site_id, location);
 }
 
 const data = fs.readFileSync('search.csv', { encoding : 'utf8' });
